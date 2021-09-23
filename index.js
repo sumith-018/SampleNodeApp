a099f0724fb1127d5273187b118c2de69303d524
sumith-018 <53033077+sumith-018@users.noreply.github.com>
2021-09-23 21:29:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.listen(3000, function () {
+app.listen(5000, function () {
 console.log("listening on 3000");
 });
 app.get("/", (req, res) => {
