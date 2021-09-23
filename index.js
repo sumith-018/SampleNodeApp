309a2598caa17cb37c68dcb12ea809679a88b559
sumith-018 <53033077+sumith-018@users.noreply.github.com>
2021-09-23 21:46:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-app.listen(5000, function () {
-console.log("listening on 5000");
+app.listen(5040, function () {
+console.log("listening on 5040");
 });
 app.get("/", (req, res) => {
 res.send("Users Shown");
