4cabae68eee34d53e36377bfc9ebac82e67b9145
sumith-018 <53033077+sumith-018@users.noreply.github.com>
2021-09-24 11:20:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,6 @@ const app = express();
 app.listen(5040, function () {
 console.log("listening on 5040");
 });
-app.get("/", (req, res) => {
-res.send("Users Shown");
-});
-app.get("/delete", (req, res) => {
-res.send("Delete User");
-});
-app.get("/update", (req, res) => {
-res.send("Update User");
-});
-app.get("/insert", (req, res) => {
-res.send("Insert User");
-});
+
+res.send(" This is Team1. ");
+res.send(" Welcome to DevOps Course ");
